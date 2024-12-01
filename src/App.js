9d05c601a0b487b5abd952e35116cc5a39922b50
BernardoBarc <bbarc67@gmail.com>
2024-12-01 15:03:52 -0300

    Commit Inicial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/header";
+import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import Login from "./pages/tarefa/Login.jsx";
+
+function App() {
+  return (
+    <div className="App">[]
+      <Login />
+      <Header />
+      <ListarTarefa />
+    </div>
+  );
+}
+
+export default App;
